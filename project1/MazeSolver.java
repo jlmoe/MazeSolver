a5cdddf8e4810e3edae2bc5d73f572e14e8d7035
Jeffrey Moe <31293711+jlmoe@users.noreply.github.com>
2017-09-11 11:30:36 -0400

    working main tester class
    This file controls the loading of the file by verifying the file's existence as well as verifying the data in the file.
    This class also tests the MazeOperations class by using the loaded data as a maze for MazeOperations to solve.

--- project1/MazeSolver.java
+++ project1/MazeSolver.java
@@ -1,10 +1,54 @@
 package project1;
 import java.util.*;
+import java.io.*;
 public class MazeSolver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner keyboard=new Scanner(System.in);
+
+		boolean goodFileName=false;
+		boolean correctSize=false;
+		boolean mazeSolved=false;
+		String fileName;
+		int[][]theMaze=new int[15][15];
+		
+
+		System.out.println("Please enter a file containing only a 15x15 matrix: ");	//user prompt
+		fileName=keyboard.nextLine();
+
+		while(!goodFileName){	//runs as long as the file is incorrect
+			try{
+				FileReader in = new FileReader(fileName);
+				Scanner dataFile=new Scanner(in);
+				try{
+					theMaze = MazeOperations.fillMaze(theMaze, dataFile);
+					goodFileName=true;//this allows us to exit the while loop
+					//we also verify the array is the correct size
+				}
+				catch(RuntimeException r){
+					System.out.println("This array is not the right size!");
+					System.out.print("Please enter a file containing only a 15x15 matrix: ");	
+				}	
+				//by this point we know the file exists
+				//now we check to see if the array is the required size
+			}
+			catch(IOException e){
+				System.out.println("This file does not exist, please try again");
+				System.out.print("Please enter a file containing only a 15x15 matrix: ");
+				fileName=keyboard.nextLine();
+			}
+
+		}//end while
+		
+		//now we have loaded the file, lets try and solve the maze
+		mazeSolved=MazeOperations.traverseMaze(theMaze,0,0);
+		MazeOperations.printMaze(theMaze);
+		
 
 	}
 
 }
+
+
+
+
