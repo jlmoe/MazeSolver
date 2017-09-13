9e8cb9299c82b43682db9bb5aa23ee3a64d7081f
Jeffrey Moe <31293711+jlmoe@users.noreply.github.com>
2017-09-13 16:28:17 -0400

    updated comments and readability

--- project1/MazeSolver.java
+++ project1/MazeSolver.java
@@ -9,6 +9,13 @@
  * 			MazeOperations- This class contains much of the functionality of the program.
  * 			It features a recursive algorithm to solve the maze (more info within the class file)
  * 			as well as printing the maze and filling the maze with data.
+ *
+ *
+ *	This class controls the inputs and outputs of the system. in main we grab a user defined file,
+ *	and then verify its existence. we then fill the maze while at the same time verifying the data in the file
+ *	There are try catch block in the program to deal with these user errors.
+ *	we then are able to manipulate the maze by putting it through the traverseMaze method and 
+ *	we can print out our results after they have been modified
  **/
 
 package project1;
@@ -17,10 +24,10 @@ import java.io.*;
 public class MazeSolver {
 
 	public static void main(String[] args) {
-		Scanner keyboard=new Scanner(System.in);
-		boolean goodFileName=false;
+		Scanner keyboard = new Scanner(System.in);
+		boolean goodFileName = false;
 		String fileName;
-		int[][]theMaze=new int[15][15];		
+		int[][]theMaze = new int[15][15];		
 
 		System.out.println("Please enter a file containing only a 15x15 matrix: ");	//user prompt
 		fileName=keyboard.nextLine();
@@ -28,10 +35,10 @@ public class MazeSolver {
 		while(!goodFileName){	//runs as long as the file is incorrect
 			try{
 				FileReader in = new FileReader(fileName);
-				Scanner dataFile=new Scanner(in);
+				Scanner dataFile = new Scanner(in);
 				try{
 					theMaze = MazeOperations.fillMaze(theMaze, dataFile);
-					goodFileName=true;//this allows us to exit the while loop
+					goodFileName = true;//this allows us to exit the while loop
 					//we also verify the array is the correct size
 				}
 				catch(RuntimeException r){
@@ -49,7 +56,7 @@ public class MazeSolver {
 		}//end while
 		
 		//now we have loaded the file, lets try and solve the maze
-		MazeOperations.traverseMaze(theMaze,0,0);
+		MazeOperations.traverseMaze(theMaze, 0, 0);
 		MazeOperations.printMaze(theMaze);
 	}
 }
