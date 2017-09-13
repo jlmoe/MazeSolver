b2c0d8582294de0c5915b5d48803292e93744c65
Jeffrey Moe <31293711+jlmoe@users.noreply.github.com>
2017-09-13 16:23:00 -0400

    updated comments and improved readability

--- project1/MazeOperations.java
+++ project1/MazeOperations.java
@@ -1,7 +1,16 @@
+/**
+  *Name: Jeffrey Moe
+  *Project 1
+  *CS 111-001
+  *September 13, 2017
+**/
+
+
 package project1;
 import java.io.FileReader;
 import java.util.*;
 /*
+ *PURPOSE
  * Traverse is the main recursive method called in the MazeSolver program
  * The traverse method finds a way through a 2D array maze
  * Requirements
@@ -20,11 +29,36 @@ import java.util.*;
  * -must not be a wall ("0")
  * Once in a new square, the square must be marked with a "3" to 
  * show it has been visited.
+ *
+ *The class MazeOperations also contains a few more functions, namely printMaze and fillMaze.
+ *printMaze displays the 15x15 maze in the console.
+ *fillMaze recieves a file to read and fills up a 15x15 array.
+ *
+ *The fillMaze may throw an exception in which there is not sufficient data to fill the maze entirely, or if the data is 
+ * too much for the array to handle.
  */
 
-/***************************
- * BASIC CONTROL STRUCTURE *
- ***************************/
+
+public class MazeOperations {
+	
+	
+	// we can assume 15X15 array
+	public static void printMaze(int[][] theMaze){
+		int row = 0;
+		int column = 0;
+		
+		for(row = 0; row < 15; row++){
+			for(column = 0; column < 15; column++){
+				System.out.print(theMaze[row][column] + " ");//print out element by element
+			}
+			System.out.println();
+		}
+	}
+	
+
+/******************************************
+ * BASIC CONTROL STRUCTURE OF traverseMaze*
+ ******************************************/
 
 /*
  * ->method call
@@ -47,86 +81,71 @@ import java.util.*;
  * 				 -> if true -> set as correct path, return true
  * 					->false -> go to next direct, if there is no next direct, return false
  */
-public class MazeOperations {
-	
-	
-	// we can assume 15X15 array
-	public static void printMaze(int[][] theMaze){
-		int row=0;
-		int column = 0;
-		
-		for(row=0;row<15;row++){
-			for(column=0;column<15;column++){
-				System.out.print(theMaze[row][column]+ " ");
-			}
-			System.out.println();
-		}
-	}
-	
-
-
 
 	public static boolean traverseMaze(int [][] maze, int row, int column){
 		
-		boolean result=false;
-		if(row==14&&column==14){	//check to see if it is the end
-			maze[row][column]=7;
-			result=true;
+		boolean result = false;
+		if(row == 14 && column == 14){	//check to see if it is the end
+			maze[row][column] = 7;
+			result = true;
 		}
 
-		else if(validSquare(maze,row,column)){//check if maze is a valid square
-			maze[row][column]=3;
+		else if(validSquare(maze, row, column)){//check if maze is a valid square
+			maze[row][column] = 3;
 			
 			//This chain of if and else if statements check each direction, if it finds a direction that is true,
 			//the function it calls will set the maze location to 7 and return back here.
-			//If the function did return true, we know we have found the base case, so we make the result true so we can backtrack
+			//If the function did return true, we know we have found the base case, 
+			//so we make the result true so we can backtrack
 			//to the correct square and confirm they are also true, causing the stack to collapse to main.
-			if(down(maze,row,column)){	//move down if possible
-				//when this is true, we break out of the if/else chain
-				result=true;	//base case found! return true
+			
+			if(down(maze, row, column)){	//move down if possible
+				result = true;	//base case found! return true
 			}
-			else if(right(maze,row,column)){	//move right if possible
-				result=true;	//base case found! return true
+			else if(right(maze, row, column)){	//move right if possible
+				result=true;	
 
 			}
-			else if(up(maze,row,column)){	//move up if possible
-				result=true;	//base case found! return true
+			else if(up(maze, row, column)){	//move up if possible
+				result = true;	
 
 			}
-			else if(left(maze,row,column)){	//move left if possible
-				result=true;	//base case found! return true
+			else if(left(maze, row, column)){	//move left if possible
+				result = true;	
 
 			}
 			else{	//if no move is possible, backtrack to previous square because there is a dead end
-				result=false;	//dead end
+				result = false;	//dead end
 			}
 		}
 		else{
 			result = false;	//backtrack to previous square because this square is invalid
 		}
-		if(row==0&&column==0){//if we are at the first activation
-			maze[row][column]=7;
+		if(row == 0 && column == 0){//if we are at the first activation
+			maze[row][column] = 7;
 		}
 		return result;	//backtrack to previous call
 	}
 	public static int[][] fillMaze(int[][] theMaze, Scanner inputFile){
-		int row=0;
-		int column=0;
+		int row = 0;
+		int column = 0;
 		while(inputFile.hasNextInt()){
-			if(row<15){//make sure the file is not overflowing with too many integers
-				theMaze[row][column]=inputFile.nextInt();
+			if(row < 15){//make sure the file is not overflowing with too many integers
+				theMaze[row][column] = inputFile.nextInt();
 			}
 			else{
 				throw new RuntimeException("YOU HAVE TOO MANY ITEMS");
 			}
-			column++;//move right
-			if(column==15){//go to next row (go down)
-				column=0;
+			column++;
+			if(column == 15){
+				column = 0;
 				row++;
 			}
 		}
-		row=0;
-		column=0;
+		if(row < 14 || column < 14){//prevents underfill
+			throw new RuntimeException("YOU HAVE NOT ENOUGH ITEMS");
+		row = 0;
+		column = 0;
 
 		return theMaze;
 	}
@@ -138,50 +157,50 @@ public class MazeOperations {
 	//these functions make this type of recursion indirect
 	private static boolean down(int [][] maze, int row, int column){	//try to move down
 
-		boolean result=false;
+		boolean result = false;
 
-		if(traverseMaze(maze,row+1,column)==true){	//if this is the correct path
-			maze[row+1][column]=7;	//mark as correct path
-			result=true;	//base case found! return true
+		if(traverseMaze(maze, row + 1, column) == true){	//if this is the correct path
+			maze[row+1][column] = 7;	//mark as correct path
+			result = true;	//base case found! return true
 		}
 		return result;
 	}
 	private static boolean right(int [][] maze, int row, int column){	//try to move right
 
-		boolean result=false;
-		if(traverseMaze(maze,row,column+1)==true){	//if this is the correct path
-			maze[row][column+1]=7;//mark as correct path
-			result=true;//base case found! return true
+		boolean result = false;
+		if(traverseMaze(maze, row, column + 1) == true){	//if this is the correct path
+			maze[row][column+1] = 7;//mark as correct path
+			result = true;//base case found! return true
 		}
 		return result;
 	}
 	private static boolean up(int [][] maze, int row, int column){	//try to move up
 
-		boolean result=false;
-		if(traverseMaze(maze,row-1,column)==true){	//if this is the correct path
-			maze[row-1][column]=7;//mark as correct path
-			result=true;//base case found! return true
+		boolean result = false;
+		if(traverseMaze(maze, row - 1, column) == true){	//if this is the correct path
+			maze[row - 1][column] = 7;//mark as correct path
+			result = true;//base case found! return true
 		}
 		return result;
 	}
 
 	private static boolean left(int [][] maze, int row, int column){	//try to move left
-		boolean result=false;
-		if(traverseMaze(maze,row,column-1)==true){
-			maze[row][column-1]=7;//mark as correct path
-			result=true;//base case found! return true
+		boolean result = false;
+		if(traverseMaze(maze, row, column - 1) == true){
+			maze[row][column - 1] = 7;//mark as correct path
+			result = true;//base case found! return true
 		}
 		return result;
 	}
 
 	private static boolean validSquare(int [][] maze, int row, int column){
-		boolean result=false;
-		if(row<15 && column<15 && row>-1 && column>-1){//check if maze is in bounds
-			if(maze[row][column]!=0 && maze[row][column] !=3){	//if it is a free square
-				result=true;
+		boolean result = false;
+		if(row < 15 && column < 15 && row > -1 && column > -1){//check if maze is in bounds
+			if(maze[row][column] !=0 && maze[row][column] != 3){	//if it is a free square
+				result = true;
 			}
 			else{	//if it is not a valid square
-				result=false;
+				result = false;
 			}
 		}
 		return result;
